fc1cb0a6c4cc956a5047ab6c149e327369acb7d5
dchakov <dchakov@yahoo.com>
2015-08-02 17:36:12 +0300

    11 finished

---
+++ 11. HTML Templates/tasks/task-3-listview.js
@@ -0,0 +1,15 @@
+(function($) {
+    $.fn.listview = function(data) {
+        var $this = $(this),
+            postTemplateHTML = $('#' + $this.attr('data-template')).html();
+        postTemplateHTML =
+            '{{#each data}}' +
+            postTemplateHTML +
+            '{{/each}}';
+        var postTemplate = handlebars.compile(postTemplateHTML);
+        $this.html(postTemplate({
+            data: data
+        }));
+        return this;
+    };
+}(JQuery));
